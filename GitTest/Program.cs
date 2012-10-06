8aed16665c2228eda73497ef97f2e4cc71a3ac1b
sasikgit <sas@kemsu.ru>
2012-10-06 22:13:54 +0700

    add for cycle

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,6 +10,10 @@ namespace GitTest
         static void Main(string[] args)
         {
             int a = 5, b = 7;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("hello world {0}", a + b - i);
+            }
             Console.WriteLine("hello world {0}", a + b);
             Console.ReadKey();
         }
